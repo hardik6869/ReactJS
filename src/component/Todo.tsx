c1d6d9547d085d225e8ed2778c2c69902df0ba9e
hardik <hardik@gmail.com>
2022-02-15 22:11:28 +0530

    solve a button style issue and try to solve a git Pull Request error

--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -5,20 +5,24 @@ import Todo_List from './Todo_List';
 const Todo = () => {
     return (
         <>
-            <div className="container">
-                <div className="row justify-content-center mt-5">
-                    <div className="box_style col-md-5 position-relative">
-                        <div className=" p-5 fw-bold text-center ">
-                            <div className="mt-3 ">
-                                <Todo_Date />
-                                <div className="col ">
-                                    <Todo_List />
+            <div className="container pb-5 mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-5">
+                        <div className="card fw-bold text-center box-style">
+                            <div className="m-5">
+                                <div className="mt-3 ">
+                                    <Todo_Date />
+                                    <div className="col ">
+                                        <Todo_List />
+                                        <div className="position-relative mr-5">
+                                            <button className="position-absolute text-muted btn_Add translate-middle">
+                                                +
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <button className="position-absolute text-muted btn_Add">
-                            +
-                        </button>
                     </div>
                 </div>
             </div>
