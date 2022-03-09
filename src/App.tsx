42b94ce204b6f5459648e4eaf715841ab78ce662
hardik <hardik@gmail.com>
2022-03-09 18:33:40 +0530

    Added Deployment File

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './components/User/About';
 import Home from './components/User/Home';
 import Services from './components/User/Services';
 import Error from './components/User/Error Page/Error';
+import Deployment from './components/User/Deployment';
 
 function App() {
     return (
@@ -12,6 +13,11 @@ function App() {
                 <nav className="navbar navbar-dark bg-light d-flex justify-content-center rounded">
                     <Link to={'/'} className="btn btn-outline-primary m-2">
                         Home
+                    </Link>
+                    <Link
+                        to={'/deployment'}
+                        className="btn btn-outline-primary m-2">
+                        Deployment
                     </Link>
                     <Link
                         to={'/services'}
@@ -26,6 +32,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/services" element={<Services />} />
+                    <Route path="/deployment" element={<Deployment />} />
                     <Route path="/about" element={<About />} />
                     <Route path="*" element={<Error />} />
                 </Routes>
---
+++ src/components/User/Deployment.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Deployment() {
+    return (
+        <>
+            <div className="container">
+                <hr />
+                <table className="table table-striped table-secondary">
+                    <tbody>
+                        <tr>
+                            <td className="p-3">Azure</td>
+                            <td>Firebase</td>
+                            <td>Github</td>
+                            <td>Netlify</td>
+                            <td>Heroku</td>
+                        </tr>
+                        <tr>
+                            <td className="p-3">
+                                <a href="https://react-deployment-app.azurewebsites.net/">
+                                    Live Demo
+                                </a>
+                            </td>
+                            <td>
+                                <a href="https://react-deployment-app.firebaseapp.com/">
+                                    Live Demo
+                                </a>
+                            </td>
+                            <td>
+                                <a href="https://hardik6869.github.io/React-Deploy-App/">
+                                    Live Demo
+                                </a>
+                            </td>
+                            <td>
+                                <a href="https://react-deployment-p7.netlify.app/">
+                                    Live Demo
+                                </a>
+                            </td>
+                            <td>
+                                <a href="https://create-deployment-app-p7.herokuapp.com/">
+                                    Live Demo
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <hr />
+            </div>
+        </>
+    );
+}
+
+export default Deployment;
